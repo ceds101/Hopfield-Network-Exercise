c4cc52b54242d54abb28b430ddac4c6b19b37f74
Barbara Jennina Perez <bbperez3@up.edu.ph>
2019-02-05 23:16:45 +0800

    reading training pattern from csv file

--- Character Recognition/main.py
+++ Character Recognition/main.py
@@ -2,15 +2,19 @@ import backend
 import numpy as np
 
 def main():
-    char_recognizer = backend.Hopfield(5)
-    char_set = np.array([[0,1,1,0,1],  #V_1
-                         [1,0,1,0,1]   #V_2
-                        ])
+    char_recognizer = backend.Hopfield(25)
+    a_file = np.genfromtxt('a_file.csv', delimiter=',', encoding="utf8")
+    print(a_file)
+    b_file = np.genfromtxt('b_file.csv', delimiter=',')
+    c_file = np.genfromtxt('c_file.csv', delimiter=',')
+
+    char_set = np.array([a_file, b_file, c_file])
 
     char_recognizer.train_network(char_set) #Train hopfield network on the given set of patterns
 
     print(char_recognizer.weight_matrix)
-    print(char_recognizer.recognize_char([1, 1, 1, 1, 1]))
+    print(char_recognizer.recognize_char(np.genfromtxt('a_file.csv', delimiter=',')))
 
 if __name__ == '__main__':
     main()
+
