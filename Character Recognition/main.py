82c31e40bb64a9633accb1cd53ea49f62a361409
Ced Rick Flores <ccflores@up.edu.ph>
2019-02-05 19:02:47 +0800

    clean

--- Character Recognition/main.py
+++ Character Recognition/main.py
@@ -3,8 +3,12 @@ import numpy as np
 
 def main():
     char_recognizer = backend.Hopfield(5)
-    print('hello world')
-    char_recognizer.train_network(np.array([[0, 1 ,1, 0,1], [1, 0, 1 ,0, 1]]))
+    char_set = np.array([[0,1,1,0,1],  #V_1
+                         [1,0,1,0,1]   #V_2
+                        ])
+
+    char_recognizer.train_network(char_set) #Train hopfield network on the given set of patterns
+
     print(char_recognizer.weight_matrix)
     print(char_recognizer.recognize_char([1, 1, 1, 1, 1]))
 
