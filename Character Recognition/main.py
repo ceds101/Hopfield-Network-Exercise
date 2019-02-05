0cf06d18b09098291c62360724afab1e96d02942
Ced Rick Flores <ccflores@up.edu.ph>
2019-02-06 00:51:35 +0800

    cleanup output

--- Character Recognition/main.py
+++ Character Recognition/main.py
@@ -1,8 +1,10 @@
 import backend
 import numpy as np
+import math
 
 def main():
-    char_recognizer = backend.Hopfield(25)
+    network_size = 25
+    char_recognizer = backend.Hopfield(network_size)
     a_file = np.genfromtxt('patterns/a_file.csv', delimiter=',').flatten()
     b_file = np.genfromtxt('patterns/b_file.csv', delimiter=',').flatten()
     c_file = np.genfromtxt('patterns/c_file.csv', delimiter=',').flatten()
@@ -11,8 +13,15 @@ def main():
     char_recognizer.train_network(char_set) #Train hopfield network on the given set of patterns
 
     #print(char_recognizer.weight_matrix)
-    in_file = np.genfromtxt('in_file.csv', delimiter=',').flatten()
-    print(char_recognizer.recognize_char(in_file))
+    in_file = np.genfromtxt('in_file.csv', delimiter=',')
+    print("Inputted Pattern:")
+    print(in_file)
+
+    out_char_vector = char_recognizer.recognize_char(in_file.flatten())
+    output = out_char_vector.view().reshape(int(math.sqrt(network_size)), int(math.sqrt(network_size))) #Reshape for readability of the result
+
+    print("\nRecognized Pattern:")
+    print(output)
 
 if __name__ == '__main__':
     main()
